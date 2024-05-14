153691ac4c51cedcf66b53b3ae9645e17dd27a31
Olga <ola.mk@mail.ru>
2024-05-15 00:08:03 +0300

    bottomNavigation bug

--- app/src/main/java/petrova/ola/playlistmaker/media/ui/media/MediaFragment.kt
+++ app/src/main/java/petrova/ola/playlistmaker/media/ui/media/MediaFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayoutMediator
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import petrova.ola.playlistmaker.R
@@ -19,6 +21,7 @@ class MediaFragment : Fragment() {
     private var _binding: FragmentMediaBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var bottomNavView: BottomNavigationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +38,8 @@ class MediaFragment : Fragment() {
             fragmentManager = childFragmentManager,
             lifecycle = lifecycle,
         )
+        bottomNavView = requireActivity().findViewById(R.id.bottomNavigationView)
+        bottomNavView.isVisible = true
 
         tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             when (position) {
